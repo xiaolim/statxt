010065e8d332392923212472fec12c1419954a56
darpan chaudhary <dc3292@columbia.edu>
2018-02-20 06:23:48 +0000

    coppied untill working of struct into a new directory

---
+++ statxt/_build/parser.mli
@@ -0,0 +1,43 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | ASSIGN
+  | NOT
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | FOR
+  | WHILE
+  | INT
+  | BOOL
+  | FLOAT
+  | VOID
+  | STRING
+  | CHAR
+  | STRUCT
+  | CHARLIT of (char)
+  | STRLIT of (string)
+  | INTLIT of (int)
+  | BLIT of (bool)
+  | ID of (string)
+  | FLIT of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
